03f2da198f734889aff63854adaa4ec89e630972
Giorgos Korfiatis <gkorf@grnet.gr>
2017-01-31 17:04:04 +0200

    Completed verifier steps

--- src/verifier.py
+++ src/verifier.py
@@ -57,6 +57,48 @@ def step4(gk, e, p2, p3, pi_c2_1, pi_c2_2, v_primes, g1rho, g2beta):
     return left == right
 
 
-def verify(
-        crs, vs, v_primes, A1, A2, pi_1sp, pi_c1_1, pi_c1_2, pi_c2_1, pi_c2_2):
-    pass
+def step5(gk, pk1, g2rho, pi_c1_1, pi_c1_2, pi_c2_1, e, p4):
+    inf1, _ = prover.get_infs(gk)
+    g1hat, h1 = pk1
+    pair1 = e(g1hat, p4[1] * pi_c1_2 + p4[2] * (pi_c1_1 + pi_c1_2))
+    pair2 = e(h1, p4[0] * pi_c1_1 + p4[1] * pi_c1_2)
+    prod = inf1
+    for (p4j, pi_c2_1j) in zip(p4, pi_c2_1):
+        prod += p4j * pi_c2_1j
+    pair3 = e(prod, g2rho)
+    return pair1 + pair2 - pair3
+
+
+def step6(gk, e, vs, v_primes, A2, p4, R, g2_polys):
+    def do_inner(vi1):
+        vi1 = vi[0]
+        inf1, _ = prover.get_infs(gk)
+        inner_prod = inf1
+        for (p4j, vi1j) in zip(p4, vi1):
+            inner_prod += p4j * vi1j
+        return inner_prod
+
+    infT = get_infT(gk)
+    outer_numer = infT
+    for (vi_prime, g2_poly_i) in zip(v_primes, g2_polys):
+        outer_numer += e(do_inner(vi_prime), g2_poly_i)
+
+    outer_denom = infT
+    for (vi, Ai2) in zip(vs, A2):
+        outer_denom += e(do_inner(vi), Ai2)
+
+    return outer_numer - outer_denom == R
+
+
+def verify(n, crs, vs, v_primes, A1, A2,
+           pi_1sp, pi_c1_1, pi_c1_2, pi_c2_1, pi_c2_2):
+    gk = crs.gk
+    A1, A2 = step1(gk, A1, A2, crs.g1_sums, crs.g2_sums)
+    p1, p2, p3, p4 = step2(gk, n)
+    perm_ok = step3(gk, gk.e, A1, A2, p1, pi_1sp,
+                    crs.g1alpha, crs.g2alpha, crs.g2rho, crs.pair_alpha)
+    valid = step4(
+        gk, gk.e, p2, p3, pi_c2_1, pi_c2_2, v_primes, crs.g1rho, crs.g2beta)
+    R = step5(gk, crs.pk1, crs.g2rho, pi_c1_1, pi_c1_2, pi_c2_1, gk.e, p4)
+    consistent = step6(gk, gk.e, vs, v_primes, A2, p4, R, crs.g2_polys)
+    return perm_ok, valid, consistent
